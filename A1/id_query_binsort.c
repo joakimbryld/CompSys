27d7fefec5e3314bcfac82ca4b9fa90bdf627b72
joakimbryld <79761631+joakimbryld@users.noreply.github.com>
2021-10-04 16:57:18 +0200

    binsort virker:D

--- A1/id_query_binsort.c
+++ A1/id_query_binsort.c
@@ -15,31 +15,33 @@ struct indexed_data {
     int n;
 };
 
-
-int compare_id (const void * a, const void * b) // fra nettet
+int compare_id(const void *p, const void *q) // fra nettet
 {
-  struct index_record *data_1 = (struct index_record *)a;
-  struct index_record *data_2 = (struct index_record *)b;
-  return ( data_1->osm_id - data_2->osm_id);
+    
+    int64_t l = ((struct index_record *)p)->osm_id;
+    int64_t r = ((struct index_record *)q)->osm_id;
+    if (l < r) {return -1; }
+    if (l > r) {return 1; }
+    return 0;
+
+    
+    
 }
-
-
 
 struct indexed_data* mk_indexedBin(struct record* rs, int n) {
 
-    struct indexed_data *data = malloc(sizeof(struct indexed_data));
-    struct index_record *irs = malloc(sizeof(struct index_record)*n);
+  struct indexed_data *data = malloc(sizeof(struct indexed_data));
+  struct index_record *irs = malloc(sizeof(struct index_record)*n);
 
-    
     for (int i = 0; i < n; i++) {
         irs[i].record = &rs[i];
         irs[i].osm_id = rs[i].osm_id;
     }
 
-    data->irs = irs;
-    data->n = n;
-    
-    return data;
+  data->irs = irs;
+  data->n = n;
+  qsort(data->irs, data->n, sizeof(struct index_record), compare_id);
+  return data;
     
 }
 
@@ -53,36 +55,26 @@ const struct record* binarySearch(struct indexed_data *data, int l, int r, int64
 {
     if (r >= l) {
         int mid = l + (r - l) / 2;
-  
-        // If the element is present at the middle
-        // itself
+
         if (data->irs[mid].osm_id == x)
             return data->irs[mid].record;
   
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
         if (data->irs[mid].osm_id > x)
             return binarySearch(data, l, mid - 1, x);
   
-        // Else the element can only be present
-        // in right subarray
         return binarySearch(data, mid + 1, r, x);
     }
-  
-    // We reach here when element is not
-    // present in array
+
     return NULL;
 }
 
 
-// https://www.geeksforgeeks.org/binary-search/
+
 
 
 const struct record* lookup_indexedBin(struct indexed_data *data, int64_t needle) {
-  if (data != NULL) {
-    qsort(data, data->n, sizeof(struct index_record), compare_id);
-    binarySearch(data, 0, data->n-1, needle);
-
+  if (data != NULL) {   
+    return (binarySearch(data, 0, data->n-1, needle));
   }
   return NULL;
 }
