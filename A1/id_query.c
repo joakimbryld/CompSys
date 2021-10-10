00eaa0141bc28050ec1df8f82b8bb44ca311e096
tgc871 <tgc871@alumni.ku.dk>
2021-10-10 14:10:02 +0200

    Final

--- A1/id_query.c
+++ A1/id_query.c
@@ -7,8 +7,10 @@
 #include "id_query.h"
 #include "timing.h"
 
-int id_query_loop(int argc, char** argv, mk_index_fn mk_index, free_index_fn free_index, lookup_fn lookup) {
-  if (argc != 2) {
+int id_query_loop(int argc, char **argv, mk_index_fn mk_index, free_index_fn free_index, lookup_fn lookup)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "Usage: %s FILE\n", argv[0]);
     exit(1);
   }
@@ -18,30 +20,35 @@ int id_query_loop(int argc, char** argv, mk_index_fn mk_index, free_index_fn fre
 
   start = microseconds();
   struct record *rs = read_records(argv[1], &n);
-  runtime = microseconds()-start;
+  runtime = microseconds() - start;
 
-  if (rs) {
-    printf("Reading records: %dms\n", (int)runtime/1000);
+  if (rs)
+  {
+    printf("Reading records: %dms\n", (int)runtime / 1000);
 
     start = microseconds();
     void *index = mk_index(rs, n);
-    runtime = microseconds()-start;
-    printf("Building index: %dms\n", (int)runtime/1000);
+    runtime = microseconds() - start;
+    printf("Building index: %dms\n", (int)runtime / 1000);
 
     char *line = NULL;
     size_t line_len;
 
     uint64_t runtime_sum = 0;
-    while (getline(&line, &line_len, stdin) != -1) {
+    while (getline(&line, &line_len, stdin) != -1)
+    {
       int64_t needle = atol(line);
 
       start = microseconds();
       const struct record *r = lookup(index, needle);
-      runtime = microseconds()-start;
+      runtime = microseconds() - start;
 
-      if (r) {
+      if (r)
+      {
         printf("%ld: %s %f %f\n", (long)needle, r->name, r->lon, r->lat);
-      } else {
+      }
+      else
+      {
         printf("%ld: not found\n", (long)needle);
       }
 
@@ -55,7 +62,9 @@ int id_query_loop(int argc, char** argv, mk_index_fn mk_index, free_index_fn fre
     free_index(index);
     free_records(rs, n);
     return 0;
-  } else {
+  }
+  else
+  {
     fprintf(stderr, "Failed to read input from %s (errno: %s)\n",
             argv[1], strerror(errno));
     return 1;
--- A1/id_query_binsort.c
+++ A1/id_query_binsort.c
@@ -5,87 +5,92 @@
 #include <string.h>
 #include <stdint.h>
 
-struct index_record {
-    int64_t osm_id;
-    const struct record *record;
+struct index_record
+{
+  int64_t osm_id;
+  const struct record *record;
 };
 
-struct indexed_data {
-    struct index_record *irs;
-    int n;
+struct indexed_data
+{
+  struct index_record *irs;
+  int n;
 };
 
 int compare_id(const void *p, const void *q) // fra nettet
 {
-    
-    int64_t l = ((struct index_record *)p)->osm_id;
-    int64_t r = ((struct index_record *)q)->osm_id;
-    if (l < r) {return -1; }
-    if (l > r) {return 1; }
-    return 0;
-    
+
+  int64_t l = ((struct index_record *)p)->osm_id;
+  int64_t r = ((struct index_record *)q)->osm_id;
+  if (l < r)
+  {
+    return -1;
+  }
+  if (l > r)
+  {
+    return 1;
+  }
+  return 0;
 }
 
-struct indexed_data* mk_indexedBin(struct record* rs, int n) {
+struct indexed_data *mk_indexedBin(struct record *rs, int n)
+{
 
   struct indexed_data *data = malloc(sizeof(struct indexed_data));
-  struct index_record *irs = malloc(sizeof(struct index_record)*n);
+  struct index_record *irs = malloc(sizeof(struct index_record) * n);
 
-    for (int i = 0; i < n; i++) {
-        irs[i].record = &rs[i];
-        irs[i].osm_id = rs[i].osm_id;
-    }
+  for (int i = 0; i < n; i++)
+  {
+    irs[i].record = &rs[i];
+    irs[i].osm_id = rs[i].osm_id;
+  }
 
   data->irs = irs;
   data->n = n;
   qsort(data->irs, data->n, sizeof(struct index_record), compare_id);
   return data;
-  
 }
 
-void free_indexedBin(struct indexed_data* data) {
-  if (data != NULL){
+void free_indexedBin(struct indexed_data *data)
+{
+  if (data != NULL)
+  {
     free(data);
   }
 }
 
-const struct record* binarySearch(struct indexed_data *data, int l, int r, int64_t x) // taget fra nettet
+const struct record *binarySearch(struct indexed_data *data, int l, int r, int64_t x) // taget fra nettet
 {
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
-        printf("%lld \n", data->irs[mid].osm_id);
-        if (data->irs[mid].osm_id == x)
-            
-            return data->irs[mid].record;
-  
-        if (data->irs[mid].osm_id > x)
-            return binarySearch(data, l, mid - 1, x);
-  
-        return binarySearch(data, mid + 1, r, x);
-    }
+  if (r >= l)
+  {
+    int mid = l + (r - l) / 2;
+    printf("%lld \n", data->irs[mid].osm_id);
+    if (data->irs[mid].osm_id == x)
 
-    return NULL;
+      return data->irs[mid].record;
+
+    if (data->irs[mid].osm_id > x)
+      return binarySearch(data, l, mid - 1, x);
+
+    return binarySearch(data, mid + 1, r, x);
+  }
+
+  return NULL;
 }
 
-
-
-
-
-const struct record* lookup_indexedBin(struct indexed_data *data, int64_t needle) {
-  if (data != NULL) {   
-    return (binarySearch(data, 0, data->n-1, needle));
+const struct record *lookup_indexedBin(struct indexed_data *data, int64_t needle)
+{
+  if (data != NULL)
+  {
+    return (binarySearch(data, 0, data->n - 1, needle));
   }
   return NULL;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   return id_query_loop(argc, argv,
-                    (mk_index_fn)mk_indexedBin,
-                    (free_index_fn)free_indexedBin,
-                    (lookup_fn)lookup_indexedBin);
+                       (mk_index_fn)mk_indexedBin,
+                       (free_index_fn)free_indexedBin,
+                       (lookup_fn)lookup_indexedBin);
 }
-
-
-
-
-
